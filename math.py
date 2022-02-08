98794367a7111ade221f8b23e04d526f8d29021a
tapasya22 <tapasya_pisar@persistent.com>
2022-02-08 23:07:54 +0530

    added function headers

--- math.py
+++ math.py
@@ -1,12 +1,12 @@
-
+#Add implementaton
 def add(x,y):
 	pass
-	
+#sub implementaton	
 def subtract(x,y):
 	pass
-    
+#multiply implementaton    
 def multiply(x,y):
 	pass
-	
+#Divide implementaton	
 def divide(x,y):
 	pass
